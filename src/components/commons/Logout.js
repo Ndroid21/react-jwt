e1dd9f6f51a8d095290b28e8439bde2adfa94d72
programmingArrow <nishant.kashyap2104@gmail.com>
2020-02-12 22:46:20 +0530

    Logout component

---
+++ src/components/commons/Logout.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+
+export default class Logout extends Component {
+    render() {
+        return <Button>Logout</Button>
+    }
+}
